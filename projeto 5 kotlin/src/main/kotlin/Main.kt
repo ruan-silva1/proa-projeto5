39b70cd642406da42a3df61a0db80c5209093cff
ruan-silva1 <ruan.jdsilva@senacsp.edu.br>
2025-03-07 23:36:07 -0300

    criando operacao bancaria em kotlin

---
+++ projeto 5 kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,96 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+var saldo = 150.0
+var extrato = DoubleArray(100)
+var qtdeExtrato = 0
+var opcao = 0
+fun main(){
+    println("qual seu nome")
+    val nome = readln()
+    println("olá, $nome!")
+    operarBanco()
+}
+
+fun operarBanco() {
+    Thread.sleep(2000)
+    println("escolha uma opção")
+    println("1. Ver saldo")
+    println("2. ver Extrato")
+    println("3. Fazer saque")
+    println("4. Fazer depósito")
+    println("5. fazerTransferência")
+    println("6. Sair")
+    opcao = readln().toInt()
+    println("a opcao selecionada foi $opcao")
+    when(opcao){
+        1 -> mostrarSaldo()
+        2 -> mostrarExtrato()
+        3 -> fazerSaque()
+        4 -> fazerDeposito()
+       // 5 -> fazerTransferencia()
+       // 6 -> encerrarPrograma()
+    }
+
+    operarBanco()
+}
+
+fun mostrarSaldo(){
+    println("seu saldo é $saldo")
+}
+
+fun validarSenha(){
+    println("coloque a senha para realizar a opcao $opcao")
+    val senha = readln()
+    if(senha != "3589"){
+        println("senha invalida")
+        validarSenha()
+    }
+}
+
+fun fazerDeposito(){
+    println("escolha um valor para deposito")
+    val valor = readln().toDouble()
+    if(valor <=0){
+        println("numero invalido!")
+        fazerDeposito()
+    }else{
+        saldo+=valor
+    }
+    extrato[qtdeExtrato] = valor
+    qtdeExtrato++
+    mostrarSaldo()
+}
+
+fun fazerSaque(){
+    println("escolha um valor para saque")
+    val saque = readln().toDouble()
+    if(saque <= 0 || saque > saldo){
+    println("operacao nao autorizada!")
+        fazerSaque()
+    }
+    else{
+        saldo -= saque
+        extrato[qtdeExtrato] = - saque
+        qtdeExtrato++
+        mostrarSaldo()
+    }
+}
+
+fun mostrarExtrato(){
+    var i=0
+    if(qtdeExtrato == 0){
+        println("nenhuma operacao realizada até o momento!")
+    }
+    while(i<qtdeExtrato){
+        if(extrato[i]<0){
+            val resultado = extrato[i] * (-1)
+            println("saida de $resultado")
+        }
+        else{
+            println("deposito de ${extrato[i]}")
+        }
+        i++
+    }
+}
